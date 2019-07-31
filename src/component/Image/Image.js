beef726ce24a8f63e1baad5bedd392438243018d
Patryk <patryk.lukasz.zych@gmail.com>
2019-08-01 00:58:11 +0200

    parallax image

--- src/component/Image/Image.js
+++ src/component/Image/Image.js
@@ -1,16 +1,27 @@
 import React, {Component} from "react";
+import ReactDOM from "react-dom";
 
 import "./image.css";
 
-export default class Image extends Component {
+class Image extends Component {
+    ref = React.createRef();
+
+    componentDidMount = () => {
+        const { current } = this.ref;
+        window.addEventListener('scroll', function(e){
+            var scrolled = window.pageYOffset;
+            var coords = '0% '+ (- (scrolled *0.3) + 'px');
+            current.style.backgroundPosition  = coords;
+          });
+    };
 
     render() {
         return (
-            <React.Fragment>
-                <div className="mainImage">
+                <div className="mainImage" ref={this.ref} >
                     <div className="textImage">WEB DEVELOPER</div>
                 </div>
-            </React.Fragment>
         )
     }
 }
+
+export default Image;
