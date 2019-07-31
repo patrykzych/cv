57727f6d619489e6f1600a0dfd50a01417a152c4
Patryk <patryk.lukasz.zych@gmail.com>
2019-08-01 00:00:42 +0200

    technology component

--- src/component/Technology/Technology.js
+++ src/component/Technology/Technology.js
@@ -1,5 +1,9 @@
 import React, {Component} from "react";
 
+import html from "../../image/html.png";
+import css from "../../image/css.png";
+import react from "../../image/react.png";
+
 import "./technology.css"
 
 export default class Technology extends Component {
@@ -8,7 +12,13 @@ export default class Technology extends Component {
         return(
             <React.Fragment>
                 <div className="technology">
-
+                    <div className="backgroundTechnology"></div>
+                    <div className="technologyText">Technology.</div>
+                    <div className="technologyContainer">
+                        <div className="technologyBlock"><div className="technologyName">html</div><img src={html} alt="html" /></div>
+                        <div className="technologyBlock"><div className="technologyName">css</div><img src={css} alt="css" /></div>
+                        <div className="technologyBlock"><div className="technologyName">react</div><img src={react} alt="react" /></div>
+                    </div>
                 </div>
             </React.Fragment>
         )
